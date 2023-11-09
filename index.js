5fb5a65be3533d3e9948f63d5ef9aa837dacaf18
pieric-ux <demont.pieric@gmail.com>
2023-11-09 14:07:27 +0000

    .

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer();
+const app = express();
 require('dotenv').config()
-
-var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
+});
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
